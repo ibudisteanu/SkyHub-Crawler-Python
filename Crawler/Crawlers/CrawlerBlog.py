c81276a1da55c2109e1fa95f98d1ca0581d64ce4
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2017-09-01 23:26:26 +0300

    small bug fix with dates

--- Crawler/Crawlers/CrawlerBlog.py
+++ Crawler/Crawlers/CrawlerBlog.py
@@ -17,7 +17,10 @@ class CrawlerBlog(CrawlerBasic):
     cssAuthor = "div.postbody"
     cssAuthorLink = "div.postauthor"
     cssFullDescription = "section p"
+
+    cssDateText = ""
     cssDate = "time::attr(datetime)"
+
     cssParent = "dd.category-name a"
 
     removeShortDescription = True
@@ -52,9 +55,20 @@ class CrawlerBlog(CrawlerBasic):
         if self.cssTitle != '':
             self.title = self.extractFirstElement(response.css(self.cssTitle))
 
-        text = ' '.join(response.css(self.cssDate).extract()).strip()
-        print(text)
-        self.date = dateparser.parse(text)
+
+
+        if self.cssDateText != '':  #text format like 22 Jul 2017
+            date = ' '.join(response.css(self.cssDate).extract()).strip()
+            print("DATEEE",date)
+            self.date = dateparser.parse(date)
+        else: #timestamp format
+            if self.extractFirstElement(response.css(self.cssDate)) == '':
+                self.fullDescription = ''
+            else:
+                self.date = self.extractFirstElement(response.css(self.cssDate))
+
+
+        self.parents = []
 
         parent = response.css(self.cssParent)
         parentText = self.extractFirstElement(parent.css('::text'))
@@ -64,7 +78,7 @@ class CrawlerBlog(CrawlerBasic):
             self.parents.append({'name': parentText, 'url': parentURL, 'index': 1})
 
     def validate(self):
-        if (len(self.title) > 3) and (len(self.fullDescription) > 10):
+        if (len(self.title) > 3) and (len(self.fullDescription) > 40):
             return 'news'
 
         return ''
