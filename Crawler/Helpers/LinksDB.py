e92c66389c5e45bfe1c6914a5ec2ce50f1e28446
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2017-09-19 15:38:31 +0300

    LinksDB allowing separate files for each website

--- Crawler/Helpers/LinksDB.py
+++ Crawler/Helpers/LinksDB.py
@@ -4,6 +4,8 @@ from Crawler.Objects.Products.ObjectProductShipping import ObjectProductShipping
 from Crawler.Objects.Products.ObjectReview import ObjectReview
 from Crawler.Objects.Products.ObjectAuthor import ObjectAuthor
 
+from Crawler.Helpers.LinksHelper import LinksHelper
+
 #import urllib.parse
 #import os.path
 from pathlib import Path
@@ -19,34 +21,41 @@ arrLinksVisited = []
 class LinksDB():
 
     def __init__(self):
-        self.readLinksFiles()
-        self.appendLinksFiles()
+        pass
 
     @staticmethod
-    def readLinksFiles():
+    def readLinksFiles(website):
 
-        print("READING LINKS FILES")
+        print("READING LINKS FILES for: ", website)
 
         global fileLinksObjects, fileLinksVisited
         global arrLinksObjects, arrLinksVisited
 
-        if Path("data/link_objects.xyz").is_file():
-            fileLinksObjects = open("data//link_objects.xyz", "rb")
-            arrLinksObjects = pickle.load(fileLinksObjects)
+        filename = "data//link-objects//"+website+".xyz"
+        if Path(filename).is_file():
+            fileLinksObjects = open(filename, "rb")
 
-        if Path("data/urls_visited.xyz").is_file():
-            fileLinksVisited = open("data//urls_visited.xyz", "r")
+            list = pickle.load(fileLinksObjects)
+
+            arrLinksObjects.append({"website":website, list:list})
+
+        filename = "data//urls-visited//" + website + ".xyz"
+        if Path(filename).is_file():
+            fileLinksVisited = open(filename, "r")
+
             content = fileLinksVisited.readlines()
-            arrLinksVisited = [x.strip() for x in content] # you may also want to remove whitespace characters like `\n` at the end of each line
+            list = [x.strip() for x in content] # you may also want to remove whitespace characters like `\n` at the end of each line
+
+            arrLinksVisited.append({"website":website, list:list})
 
     @staticmethod
-    def appendLinksFiles():
+    def appendLinksFiles(website):
         global arrLinksObjects, fileLinksVisited
-        fileLinksVisited = open("data//urls_visited.xyz", "a")
+        fileLinksVisited = open("data//urls_visited//"+website+".xyz", "a")
 
     @staticmethod
-    def findLinkObjectAlready( url='', title='', allowTitleIncluded=False):
-        url = LinksDB.fix_url(url)
+    def findLinkObjectAlready(website, url='', title='', allowTitleIncluded=False):
+        url = LinksHelper.fix_url(url)
 
         global arrLinksObjects
         for object in arrLinksObjects:
@@ -59,8 +68,8 @@ class LinksDB():
         return None
 
     @staticmethod
-    def checkLinkVisitedAlready(url):
-        url = LinksDB.fix_url(url)
+    def checkLinkVisitedAlready(website, url):
+        url = LinksHelper.fix_url(url)
 
         global arrLinksVisited
         if url in arrLinksVisited:
@@ -69,7 +78,7 @@ class LinksDB():
 
     @staticmethod
     def addLinkVisited(url):
-        url = LinksDB.fix_url(url)
+        url = LinksHelper.fix_url(webiste, url)
 
         global arrLinksVisited
         global fileLinksVisited
@@ -77,7 +86,7 @@ class LinksDB():
         fileLinksVisited.write(url)
 
     @staticmethod
-    def addLinkObject(object):
+    def addLinkObject(website, object):
 
         global arrLinksObjects
         global fileLinksObjects
@@ -89,71 +98,3 @@ class LinksDB():
         fileLinksObjects.close()
 
 
-
-
-
-    @staticmethod
-    def fix_url(url):   # based on https://stackoverflow.com/questions/32178535/repair-url-using-python
-        url = url.lower()
-
-        if url.find("https") >= 0:
-            url = url.replace("https", "http")
-
-        if url.find("http") < 0:
-            url.replace("://", "")
-            url = "http://"+url
-
-        if (url.find("www.") >= 0) and (url.find("www.") < 10):
-            url = url.replace("www.", "")
-
-        url = url.split("#", 1)[0]
-
-        return url
-
-    @staticmethod
-    def fix_relative_urls(text, rootURL):
-        # src='/img' pr href="/"
-
-        text = text.replace("src='/", "src='"+rootURL+'/')
-        text = text.replace('src="/', 'src="'+rootURL+'/')
-
-        text = text.replace("href='/", "href='"+rootURL+'/')
-        text = text.replace('href="/', 'href="'+rootURL+'/')
-
-        return text
-
-
-    @staticmethod
-    def fixArchiveStrings(text):
-
-        # https://web.archive.org/web/20130502222444/
-        if "web.archive.org/web/" in text:
-            positionStart = text.index("web.archive.org/web/")
-            text = text[positionStart + len("web.archive.org/web/20130502222444/"):10000]
-
-        if "http" in text:
-            positionStart = text.index("http")
-            if positionStart != 0:
-                text = text[positionStart:10000]
-
-        return text
-
-
-
-    @staticmethod
-    def getRequestTrials(session, url,  data={}, headers={}, maxTrials = 5):
-        error = True
-        trials = 0
-        while error == True and trials < maxTrials:
-            error = False
-            trials += 1
-            try:
-                response = session.get(url , data=data, headers=headers)
-                if response.status_code == 503:
-                    response.raise_for_status()
-            except requests.exceptions.HTTPError as e:
-                if e.response.status_code == 503:
-                    error = True
-                # handle your 503 specific error
-
-        return response
